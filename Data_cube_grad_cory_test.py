corrected indexing and iterating in gradient for loop

Data_cube_grad_cory_test.py
@@ -10,7 +10,7 @@ elevMatrix = np.transpose(np.transpose(elevTensor)[2][:])
 numElevMatrixRows = np.shape(elevMatrix)[0] # Number of rows
 numElevMatrixCols = np.shape(elevMatrix)[1] # Number of columns
 
-# Create shell matrix. 
+# Create shell matrix.
 shellMatrix = np.full(((numElevMatrixRows + 2), (numElevMatrixCols + 2)), -50000.0, dtype=float)
 
 # Put the shellMatrix around the core matrix
@@ -26,9 +26,9 @@ gradTensor = np.resize(elevTensor, (numElevMatrixRows, numElevMatrixCols, 11))
 
 # Take all the differences
 row = 1 # Row counter
-while row <= numElevMatrixRows + 1:
+while row <= numElevMatrixRows:
     column = 1 # Column counter
-    while column <= numElevMatrixCols + 1:
+    while column <= numElevMatrixCols:
         height = 0
         while height < 8:
             GN = shellMatrix[row][column] - shellMatrix[row-1][column] #Up
@@ -40,7 +40,7 @@ while row <= numElevMatrixRows + 1:
             GW = shellMatrix[row][column] - shellMatrix[row][column-1] #Left
             GNW = shellMatrix[row][column] - shellMatrix[row-1][column-1] #Diagonal Up Left
             gradList = [GN, GNE, GE, GSE, GS, GSW, GW, GNW]
-            gradTensor[row-1][column-1][height] = gradList[height+3]
+            gradTensor[row-1][column-1][height+3] = gradList[height]
             height += 1
         column += 1
     row += 1