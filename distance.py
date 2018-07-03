1bf1a63fc97952408faa3550812ce1f79a8ba2ca
Kennedy <ckennedy@ckennedy-23233s.nrel.gov>
2018-07-03 15:59:03 -0600

    restructured and organized repo

--- distance.py
+++
@@ -1,9 +0,0 @@
-import earthly as ea
-from math import sqrt
-
-def distance_between_points(pnt_1, pnt_2):
-    lon_dist = ea.parallelDistance(pnt_1[0], pnt_2[0])
-    lat_dist = ea.meridianDistance(pnt_1[0], pnt_1[1], pnt_2[0], pnt_2[1])
-    return sqrt(lat_dist**2 + lon_dist**2)
-
-
--- terrainViewer.py
+++
@@ -1,6 +0,0 @@
-import Earthly as ea # import my awesome library
-
-SouthTable = ea.Region('NTable') # create a region object
-SouthTable.add_CoordLayer(5) # add 2D coordinate layer data member
-SouthTable.add_ElevLayer() # add 2D elevation layer data member
-SouthTable.ElevVis() # generate visualization library using plotly
