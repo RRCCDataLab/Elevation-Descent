086b56b5cb7053a734b5d920a14ac5bb4cb3443d
Cory Kennedy <cory.kennedy@nrel.gov>
2018-08-01 14:23:15 -0600

    added a config file for storing filepaths; enables crossplatform usability

---
+++ src/config.py
@@ -0,0 +1,16 @@
+#
+# CONFIG FILE FOR CKENNEDY'S MACBOOK
+#
+# Modify and store your own, untracked version to use locally
+#
+# 08/01/2018
+
+TSDC_CONFIG = {
+}
+
+CFDS_CONFIG = {
+        'path_10m_data': '/Volumes/Fleet Storage/DEM_Database/NED_13/grid/',
+        'path_1m_data': '/Volumes/Fleet Storage/DEM_Database/NED_1m/',
+        'path_1m_meta': '/Volumes/Fleet Storage/DEM_Database/NED_1m/',
+}
+
