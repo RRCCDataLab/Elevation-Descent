ab8203ca7e7f9d3fa49f4fa00c68576a5430eae9
Cory Kennedy <cory.kennedy@nrel.gov>
2018-08-01 15:32:03 -0600

    changed config file for TSDC settings

--- src/config.py
+++ src/config.py
@@ -1,16 +1,23 @@
 #
-# CONFIG FILE FOR CKENNEDY'S MACBOOK
+# CONFIG FILE FOR TSDC SERVER
 #
 # Modify and store your own, untracked version to use locally
 #
 # 08/01/2018
 
+from os import path
+
+path_to_DEMs = path.join('Z:', 'DEM_Database')
+
 TSDC_CONFIG = {
 }
 
 CFDS_CONFIG = {
-        'path_10m_data': '/Volumes/Fleet Storage/DEM_Database/NED_13/grid/',
-        'path_1m_data': '/Volumes/Fleet Storage/DEM_Database/NED_1m/',
-        'path_1m_meta': '/Volumes/Fleet Storage/DEM_Database/NED_1m/',
+        'path_10m_data': path.join(path_to_DEMs, 'NED_13', 'grid'),
+        'path_1m_data': path.join(path_to_DEMs, 'NED_1m'),
+        'path_1m_meta': path.join(path_to_DEMs, 'NED_1m'),
 }
 
+GDAL_CONFIG = {
+        'path_to_gdal_merge': path.join('C:', 'Program Files (x86)', 'GDAL')
+}
