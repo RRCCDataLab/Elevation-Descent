c53d9171d577e1ac6dd4475e2c177439746de2e3
Cory Kennedy <cory.kennedy@nrel.gov>
2018-08-01 16:32:39 -0600

    added gdal_merge.py to local repository

---
+++ src/gdal_merge.py
@@ -0,0 +1,4 @@
+#!/Users/ckennedy/miniconda2/envs/tensorflow/bin/python
+# EASY-INSTALL-SCRIPT: 'GDAL==2.2.4','gdal_merge.py'
+__requires__ = 'GDAL==2.2.4'
+__import__('pkg_resources').run_script('GDAL==2.2.4', 'gdal_merge.py')
